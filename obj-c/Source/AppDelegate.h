ebb66bea7cf8913fcc69f5d33323c509c5f36fb5
Alexander Kremenets <kremenets0207@gmail.com>
2020-02-18 13:12:37 +0100

    fix: obj-c project fixed and compiles

--- obj-c/Source/AppDelegate.h
+++ obj-c/Source/AppDelegate.h
@@ -1,17 +1,17 @@
 //
 //  AppDelegate.h
-//  StreamLayer-sample-Objective-C
+//  Sample2
 //
-//  Created by Alexander Kremenets on 27.01.2020.
+//  Created by Alexander Kremenets on 18.02.2020.
 //  Copyright © 2020 Alexander Kremenets. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-#import <WebRTC/WebRTC.h>
-#import <StreamLayer/StreamLayer.h>
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
 
 @end
 
