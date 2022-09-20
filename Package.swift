a82295c2f4c09d2095eb8a88c26f9fd2f4f3392b
bimawa <bimawa@gmail.com>
2022-09-20 11:59:56 +0500

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "StreamLayerSDK",
+    name: "StreamLayerPackage",
     platforms: [
         .iOS(.v15),
     ],
     products: [
         .library(
-            name: "StreamLayerSDK",
+            name: "StreamLayerLibrary",
             targets: ["StreamLayerWrapper"]),
     ],
     dependencies: [
@@ -36,18 +36,18 @@ let package = Package(
             ]),
         .binaryTarget(
             name: "StreamLayer",
-            url: "https://storage.googleapis.com/ios.streamlayer.io/v8.1.48/StreamLayerSDK.xcframework.zip",
-            checksum: "8bcc0bb9d3c66ebb11290462feb7b1f91695407dc81245e80867d9aa88104ef7"
+            url: "https://storage.googleapis.com/ios.streamlayer.io/v8.1.50/StreamLayer.xcframework.zip",
+            checksum: "f22f8521d9247d47c61432c620888e3d596fea97f1167c329799325c004987db"
         ),
         .binaryTarget(
             name: "SLProtofiles",
-            url: "https://storage.googleapis.com/ios.streamlayer.io/v8.1.48/SLProtofiles.xcframework.zip",
-            checksum: "45ebf88fe35efa9587b539b86d4889931aa0c3c28377b773131c7ae867ec66ad"
+            url: "https://storage.googleapis.com/ios.streamlayer.io/v8.1.50/SLProtofiles.xcframework.zip",
+            checksum: "fd4211d32e5f3f79d77f927bdea94ae445f3aacc2dae54c98e0d7b16a5b44a23"
         ),
         .binaryTarget(
             name: "PromiseKit_34A278A94EE8AA11_PackageProduct",
-            url: "https://storage.googleapis.com/ios.streamlayer.io/v8.1.48/PromiseKit_34A278A94EE8AA11_PackageProduct.xcframework.zip",
-            checksum: "dfb2af29cd1f74f8163b968db2542479f36da25baa150ee0b09f9fd3c2d13baa"
+            url: "https://storage.googleapis.com/ios.streamlayer.io/v8.1.50/PromiseKit_34A278A94EE8AA11_PackageProduct.xcframework.zip",
+            checksum: "06632d9c372a192350fc1dc0eb8fad24250898e93e904b4beddae66526f74253"
         ),
     ]
 )
