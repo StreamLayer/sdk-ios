86830ec44cd3ee03b6e497660a2d953bbb6ff5f4
bimawa <bimawa@gmail.com>
2023-02-10 01:24:56 +0500

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
       ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.6.109/StreamLayerSDK.xcframework.zip",
-          checksum: "615fdc8504817aba595d861d0e8733d96ecd1b4086d2f980ccaeafc2025638a9"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.6.110/StreamLayerSDK.xcframework.zip",
+          checksum: "c8804dc79f512becd2f1823302ff7d354f55b2ac56df31a0f93ac2a8bada2b96"
       ),
       .binaryTarget(
           name: "OpenTok",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.6.109/OpenTok.xcframework.zip",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.6.110/OpenTok.xcframework.zip",
           checksum: "b9b8ba4d28802acf3061b5e09f3a32928ecd068f866ad46d010a8e1528aed8ad"
       ),
     ]
