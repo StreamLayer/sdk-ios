0a40ff09fca324d5179602ca967ddd357e3ecc54
bimawa <bimawa@gmail.com>
2023-07-19 14:05:16 +0500

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
       ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.5/StreamLayerSDK.xcframework.zip",
-          checksum: "b2ae9827008178642ac027c92f8d29a62dc29ec4314489796396b118a5bdea2f"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.6/StreamLayerSDK.xcframework.zip",
+          checksum: "beaae40681751648f0e764c20e192066f2fe3bea7b8d28f256a8f071f6c8feab"
       ),
     ]
 )
