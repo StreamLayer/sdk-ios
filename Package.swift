2b9a8e6751e96a8834c2bab38f55fcc86968c971
bimawa <bimawa@gmail.com>
2023-07-19 18:28:58 +0500

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -22,18 +22,18 @@ let package = Package(
       ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.9/StreamLayerSDK.xcframework.zip",
-          checksum: "1b87134f7d7517077e03661bd37863d452f5fcdbbeb4eff243acae355714977e"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.10/StreamLayerSDK.xcframework.zip",
+          checksum: "136d451ebebd6ba5a4f0f72b7ba5adb44536a0c3d9b691c2a1de9d132374b61b"
       ),
       .binaryTarget(
-          name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.9/SLRStorageFramework.xcframework.zip",
-          checksum: "8215b04d902a774c8187ede7c8e916a58419d55920ce1917ce7d81d3c2c99dd4"
+          name: "SLRStorageFramework",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.10/SLRStorageFramework.xcframework.zip",
+          checksum: "d49ddfec2f847610dd1b39f5b36ec41bb8045a73bb2ed5ccba363642fede6425"
       ),
       .binaryTarget(
-          name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.9/SLRUtilsFramework.xcframework.zip",
-          checksum: "4656b1a989e17af47353209c8fa564ac87e6621b737d92d3360ec2b8e90627c2"
+          name: "SLRUtilsFramework",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.10/SLRUtilsFramework.xcframework.zip",
+          checksum: "76b7e36f4cddc9952dda2ed8cf348f91076ed1f15a0c71c1f65b7fc08792f7be"
       ),
     ]
 )
