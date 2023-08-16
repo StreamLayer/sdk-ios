6ae365e889304b6d3670852f429a3848614fcba4
bimawa <bimawa@gmail.com>
2023-08-16 17:39:12 +0500

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -1,7 +1,6 @@
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// swift-module-flags: -target arm64-apple-ios15.0
-// OTHER_LDFLAGS = -weak_framework AVFAudio
+// swift-module-flags: -target arm64-apple-ios13.0
 
 import PackageDescription
 
@@ -21,28 +20,22 @@ let package = Package(
             .target(name: "SLRStorageFramework"),
             .target(name: "SLRUtilsFramework"),
           ],
-          path: "SwiftPM-PlatformExclude/StreamLayer",
-          linkerSettings: [
-            .linkedFramework("AVFAudio"),
-            .linkedFramework("AudioToolbox"),
-            .linkedFramework("AVFoundation"),
-            .linkedLibrary("c++")
-          ]
+          path: "SwiftPM-PlatformExclude/StreamLayer"
       ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.31/StreamLayerSDK.xcframework.zip",
-          checksum: "ad8970188b2595252b5315a1dff3d0dbb54253ec30bb4fbb64c3ebad63738dcf"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.32/StreamLayerSDK.xcframework.zip",
+          checksum: "87cf7e1fe1936f24bcdc367e470c004c9d09404bb2c8c35efcb3c3a278416d04"
       ),
       .binaryTarget(
           name: "SLRStorageFramework",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.31/SLRStorageFramework.xcframework.zip",
-          checksum: "af2cea344e575497eb68acc5e1d757fce47271272172087d6badc543861b2c95"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.32/SLRStorageFramework.xcframework.zip",
+          checksum: "081f03cdfed4ad1d169ed41ce38a61a44ebb864b5c5d5adaa7c42e3a846e0d98"
       ),
       .binaryTarget(
           name: "SLRUtilsFramework",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.31/SLRUtilsFramework.xcframework.zip",
-          checksum: "2d1f67ad8d8ccfe746ae8cc13c2c0b4c6d11e97d4e3cc50fd0588b2e855639b9"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.14.32/SLRUtilsFramework.xcframework.zip",
+          checksum: "9eed5641a885ceaa6dbce926cb5cbd206eebfa956b8d344985ec1b7fbd8219af"
       ),
     ]
 )
