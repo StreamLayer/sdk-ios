16749297acf549307fc54913db5092356cc33bc3
StreamLayer, Inc <support@streamlayer.io>
2025-01-20 09:18:41 +0000

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // swift-module-flags: -target arm64-apple-ios14.0
 
@@ -10,7 +10,8 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        .library(name: "StreamLayer", targets: ["StreamLayer"])
+        .library(name: "StreamLayer", targets: ["StreamLayer"]),
+        .library(name: "StreamLayerWatchParty", targets: ["StreamLayerWatchParty"])
     ],
     targets: [
       .target(
@@ -20,10 +21,22 @@ let package = Package(
           ],
           path: "SwiftPM-PlatformExclude/StreamLayer"
       ),
+      .target(
+          name: "StreamLayerWatchParty",
+          dependencies: [
+            .target(name: "StreamLayerSDKWatchParty")
+          ],
+          path: "SwiftPM-PlatformExclude/StreamLayerWatchParty"
+      ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.22.28/StreamLayerSDK.xcframework.zip",
-          checksum: "43fd5e0a44dc488460a630041c08583075fee0cd55afae81087966e299ca8e54"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/33340/StreamLayerSDK.xcframework.zip",
+          checksum: "f92a9b92679e2ea436918d5cebda911240dc98e867b6c78e4beaf0ae047e6bc0"
+      ),
+      .binaryTarget(
+          name: "StreamLayerSDKWatchParty",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/33340/StreamLayerSDK.xcframework.zip",
+          checksum: "f92a9b92679e2ea436918d5cebda911240dc98e867b6c78e4beaf0ae047e6bc0"
       )
     ]
 )
