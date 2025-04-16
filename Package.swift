65dffd8120ad69a37de95b658274bab78e9eb0d0
StreamLayer, Inc <support@streamlayer.io>
2025-04-16 11:13:37 +0000

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // swift-module-flags: -target arm64-apple-ios14.0
 
@@ -10,7 +10,12 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        .library(name: "StreamLayer", targets: ["StreamLayer"])
+        .library(name: "StreamLayer", targets: ["StreamLayer"]),
+        .library(name: "StreamLayerWatchParty", targets: ["StreamLayerWatchParty"]),
+        .library(name: "StreamLayerGooglePAL", targets: ["StreamLayerGooglePAL"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-programmatic-access-library-ios", exact: "2.8.1"),
     ],
     targets: [
       .target(
@@ -20,10 +25,35 @@ let package = Package(
           ],
           path: "SwiftPM-PlatformExclude/StreamLayer"
       ),
+      .target(
+          name: "StreamLayerWatchParty",
+          dependencies: [
+            .target(name: "StreamLayerSDKWatchParty")
+          ],
+          path: "SwiftPM-PlatformExclude/StreamLayerWatchParty"
+      ),
+      .target(
+          name: "StreamLayerGooglePAL",
+          dependencies: [
+            .target(name: "StreamLayerSDKGooglePAL"),
+            .product(name: "ProgrammaticAccessLibrary", package: "swift-package-manager-google-programmatic-access-library-ios"),
+          ],
+          path: "SwiftPM-PlatformExclude/StreamLayerGooglePAL"
+      ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.22.28/StreamLayerSDK.xcframework.zip",
-          checksum: "43fd5e0a44dc488460a630041c08583075fee0cd55afae81087966e299ca8e54"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/33983/StreamLayerSDK.xcframework.zip",
+          checksum: "7e930c8ecfb30c43ced405ab0ba73f03723a1a37d8dddf5ca9866295a929727a"
+      ),
+      .binaryTarget(
+          name: "StreamLayerSDKWatchParty",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/33983/StreamLayerSDKWatchParty.xcframework.zip",
+          checksum: "310d9b43d8b0e74cb0c7325dc97c742c6d333b9195b4cee10557efb70c5fdc6d"
+      ),
+      .binaryTarget(
+          name: "StreamLayerSDKGooglePAL",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/33983/StreamLayerSDKGooglePAL.xcframework.zip",
+          checksum: "48de0b1b7954b16834d237c23aa7510d08fe706891b74a7f48d36788c8ad1a50"
       )
     ]
 )
