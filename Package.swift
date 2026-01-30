9ebeaec80ecfe67d1acb9935a56281994beaf603
StreamLayer, Inc <support@streamlayer.io>
2026-01-30 08:11:56 +0000

    feat(sdk): next version

--- Package.swift
+++ Package.swift
@@ -1,16 +1,21 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// swift-module-flags: -target arm64-apple-ios14.0
 
 import PackageDescription
 
 let package = Package(
     name: "StreamLayer",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14), .tvOS(.v15)
     ],
     products: [
-        .library(name: "StreamLayer", targets: ["StreamLayer"])
+        .library(name: "StreamLayer", targets: ["StreamLayer"]),
+        .library(name: "StreamLayerTVOS", targets: ["StreamLayerTVOS"]),
+        .library(name: "StreamLayerWatchParty", targets: ["StreamLayerWatchParty"]),
+        .library(name: "StreamLayerGooglePAL", targets: ["StreamLayerGooglePAL"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/StreamLayer/sl-programmatic-access-library-ios-tvos", exact: "2.8.1"),
     ],
     targets: [
       .target(
@@ -20,10 +25,47 @@ let package = Package(
           ],
           path: "SwiftPM-PlatformExclude/StreamLayer"
       ),
+      .target(
+          name: "StreamLayerTVOS",
+          dependencies: [
+            .target(name: "StreamLayerSDKTVOS")
+          ],
+          path: "SwiftPM-PlatformExclude/StreamLayerTVOS"
+      ),
+      .target(
+          name: "StreamLayerWatchParty",
+          dependencies: [
+            .target(name: "StreamLayerSDKWatchParty")
+          ],
+          path: "SwiftPM-PlatformExclude/StreamLayerWatchParty"
+      ),
+      .target(
+          name: "StreamLayerGooglePAL",
+          dependencies: [
+            .target(name: "StreamLayerSDKGooglePAL"),
+            .product(name: "ProgrammaticAccessLibrary", package: "sl-programmatic-access-library-ios-tvos"),
+          ],
+          path: "SwiftPM-PlatformExclude/StreamLayerGooglePAL"
+      ),
       .binaryTarget(
           name: "StreamLayerSDK",
-          url: "https://storage.googleapis.com/ios.streamlayer.io/v8.22.28/StreamLayerSDK.xcframework.zip",
-          checksum: "43fd5e0a44dc488460a630041c08583075fee0cd55afae81087966e299ca8e54"
+          url: "https://storage.googleapis.com/ios.streamlayer.io/35542/StreamLayerSDK.xcframework.zip",
+          checksum: "d4f9078e03f25862b608bebb0051c448b3db170c4ddf68bb9558ab1273738a77"
+      ),
+      .binaryTarget(
+          name: "StreamLayerSDKTVOS",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/35542/StreamLayerSDKTVOS.xcframework.zip",
+          checksum: "31a01df3c928879188f30221feec1f7882af31cb89b99a7142c3712c8fa490cc"
+      ),
+      .binaryTarget(
+          name: "StreamLayerSDKWatchParty",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/35542/StreamLayerSDKWatchParty.xcframework.zip",
+          checksum: "53a47f2d9e73037539e706ef246659d5e20c0d8bfec3b543b19369a5d91cebd8"
+      ),
+      .binaryTarget(
+          name: "StreamLayerSDKGooglePAL",
+          url: "https://storage.googleapis.com/ios.streamlayer.io/35542/StreamLayerSDKGooglePAL.xcframework.zip",
+          checksum: "372c99925d40cf42d2b164619eee3783bc45bddc203a14f39237a32eb11b6366"
       )
     ]
 )
